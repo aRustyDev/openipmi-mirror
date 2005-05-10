1573923ae0b7cf57e92328e41301b83c5d87bb99
cminyard <cminyard@a860ddb0-99b4-4eb6-8282-105f033f2ff5>
2005-05-10 14:21:30 +0000

    Fix an overflow.
    git-svn-id: file:///home/cminyard/t2/svn/openipmi/branches/rel_1_3@1275 a860ddb0-99b4-4eb6-8282-105f033f2ff5

--- ui/ui.c
+++ ui/ui.c
@@ -4099,7 +4099,7 @@ clearpeflock_cmd(char *cmd, char **toks, void *cb_data)
 
     mc_toks = strtok_r(NULL, "", toks);
     if (mc_toks) {
-	strncpy(buf+2, mc_toks, sizeof(buf));
+	strncpy(buf+2, mc_toks, sizeof(buf)-2);
 	buf[0] = 'a';
 	buf[1] = ' ';
 	strtok_r(buf, " ", &ntoks);
@@ -4706,7 +4706,7 @@ clearlanparmlock_cmd(char *cmd, char **toks, void *cb_data)
 
     mc_toks = strtok_r(NULL, "", toks);
     if (mc_toks) {
-	strncpy(buf+2, mc_toks, sizeof(buf));
+	strncpy(buf+2, mc_toks, sizeof(buf)-2);
 	buf[0] = 'a';
 	buf[1] = ' ';
 	strtok_r(buf, " ", &ntoks);
