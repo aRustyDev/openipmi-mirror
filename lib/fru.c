6ce5d88ca05990984e65ccfd99a6f9fad2fb810a
cminyard <cminyard@a860ddb0-99b4-4eb6-8282-105f033f2ff5>
2005-06-02 13:50:42 +0000

    Fix some FRU handling problems.
    git-svn-id: file:///home/cminyard/t2/svn/openipmi/branches/rel_1_4@1285 a860ddb0-99b4-4eb6-8282-105f033f2ff5

--- lib/fru.c
+++ lib/fru.c
@@ -302,9 +302,9 @@ read_fru_time(unsigned char **data,
     if (*len < 3)
 	return EBADF;
 
-    t = *d++ * 256 * 256;
+    t = *d++;
     t += *d++ * 256;
-    t += *d++;
+    t += *d++ * 256 * 256;
 
     *len -= 3;
     *data += 3;
@@ -3540,6 +3540,7 @@ start_domain_fru_write(ipmi_domain_t *domain, void *cb_data)
 	/* No data changed, no write is needed. */
 	ipmi_mem_free(fru->data);
 	fru->data = NULL;
+	fru->in_use = 0;
 	fru_unlock(fru);
 
 	if (fru->domain_fetched_handler)
